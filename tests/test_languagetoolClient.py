779570ec4e53bdd7ca1a1e3863ce24cb4e8eadc3
Caleb Scott <duckduckdoof@gmail.com>
2024-05-16 11:12:44 -0400

    Added test for multiple sequential calls to ltServer

--- tests/test_languagetoolClient.py
+++ tests/test_languagetoolClient.py
@@ -21,7 +21,36 @@ from awe_languagetool.languagetoolClient import mappingPathError
 # This describes the desired mapping path pointing to a rulemapping.json file
 EXPECTED_RULEMAP_PATH = "awe_languagetool/languagetool_rulemapping.json"
 
-LION_TEXT = "test_texts/lion.txt"
+TEXT_BASE = "test_texts/"
+LION_TEXT = TEXT_BASE + "lion.txt"
+
+CENSORSHIP_TEXTS = [
+    "censorship1.txt",
+    "censorship2.txt",
+    "censorship3.txt",
+    "censorship4.txt",
+    "censorship5.txt",
+    "censorship6.txt",
+    "censorship7.txt",
+    "censorship8.txt",
+    "censorship9.txt",
+    "censorship10.txt",
+    "censorship11.txt",
+    "censorship12.txt",
+    "censorship13.txt",
+    "censorship14.txt",
+    "censorship15.txt",
+    "censorship16.txt",
+    "censorship17.txt",
+    "censorship18.txt",
+    "censorship19.txt",
+    "censorship20.txt",
+    "censorship21.txt",
+    "censorship22.txt",
+    "censorship23.txt",
+    "censorship24.txt",
+    "censorship25.txt"
+]
 
 # --- [ SETUP ] --------------------------------------------------------------------
 
@@ -42,19 +71,22 @@ class LanguageToolClientTest(unittest.TestCase):
         """
         try:
             new_client = languagetoolClient.languagetoolClient()
-            self.assertTrue(new_client.MAPPING_PATH.endswith(EXPECTED_RULEMAP_PATH))
+            self.assertTrue(
+                new_client.MAPPING_PATH.endswith(EXPECTED_RULEMAP_PATH)
+            )
         except mappingPathError as e:
             self.fail()
 
-    def test_client_timing_java(self):
+    def test_client_timing_java_single(self):
         """
         Attempt to start up a client, and pass in a sample text.
-        We are measuring runtime of how well the server responds to a single request.
+        We are measuring runtime of how well the server responds to a 
+        single request.
 
         Precondition: languagetoolServer is running.
         """
         try:
-            new_client = languagetoolClient.languagetoolClient()
+            client = languagetoolClient.languagetoolClient()
             with open(LION_TEXT, 'r') as text_file:
 
                 # Grab the text
@@ -62,7 +94,7 @@ class LanguageToolClientTest(unittest.TestCase):
 
                 # Start timing benchmark
                 start = time.process_time()
-                output = asyncio.run(new_client.summarizeText(sample_text))
+                output = asyncio.run(client.summarizeText(sample_text))
                 end = time.process_time()
 
                 # Show results
@@ -70,6 +102,44 @@ class LanguageToolClientTest(unittest.TestCase):
                 print("---------[ TIMING BENCHMARK ]---------")
                 print(f"Time Elapsed: {end - start}")
                 print("--------------------------------------")
+        except mappingPathError as e:
+            self.fail()
+
+    def test_client_timing_java_multiple_sequential(self):
+        """
+        Attempt to start up a client, and pass in a sample text.
+        We are measuring runtime of how well the server responds to
+        many sequential requests.
+
+        Precondition: languagetoolServer is running.
+        """
+        try:
+            client = languagetoolClient.languagetoolClient()
+
+            # Pre-load all texts
+            all_texts = {}
+            index = 0
+            for text_filename in CENSORSHIP_TEXTS:
+                with open(TEXT_BASE + text_filename) as censorship_file:
+                    all_texts[index] = censorship_file.read()
+                    index = index + 1
+
+            # Make multiple requests
+            start = time.process_time()
+            output = asyncio.run(
+                client.summarizeMultipleTexts(
+                    list(all_texts.keys()), 
+                    list(all_texts.values())
+                )
+            )
+            end = time.process_time()
+
+            # Show results
+            print()
+            print("---------[ TIMING BENCHMARK ]---------")
+            print(f"Time Elapsed: {end - start}")
+            print("--------------------------------------")
+
         except mappingPathError as e:
             self.fail()
 
