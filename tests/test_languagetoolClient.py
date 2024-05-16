c665498bb58a7bb6c776691456a3bfa91b604bdf
Caleb Scott <duckduckdoof@gmail.com>
2024-05-16 10:20:34 -0400

    Added client test for prelim server runtime testing

--- tests/test_languagetoolClient.py
+++ tests/test_languagetoolClient.py
@@ -10,6 +10,7 @@
 # --- [ IMPORTS ] ------------------------------------------------------------------
 
 import unittest
+import time
 
 from awe_languagetool import languagetoolClient
 from awe_languagetool.languagetoolClient import mappingPathError
@@ -18,6 +19,8 @@ from awe_languagetool.languagetoolClient import mappingPathError
 
 # This describes the desired mapping path pointing to a rulemapping.json file
 EXPECTED_RULEMAP_PATH = "awe_languagetool/languagetool_rulemapping.json"
+
+LION_TEXT = "test_texts/lion.txt"
 
 # --- [ SETUP ] --------------------------------------------------------------------
 
@@ -32,9 +35,39 @@ EXPECTED_RULEMAP_PATH = "awe_languagetool/languagetool_rulemapping.json"
 class LanguageToolClientTest(unittest.TestCase):
 
     def test_client_init(self):
+        """
+        Basic test to see if the client properly aligns its paths 
+        for files in its package.
+        """
         try:
             new_client = languagetoolClient.languagetoolClient()
             self.assertTrue(new_client.MAPPING_PATH.endswith(EXPECTED_RULEMAP_PATH))
+        except mappingPathError as e:
+            self.fail()
+
+    def test_client_timing_java(self):
+        """
+        Attempt to start up a client, and pass in a sample text.
+        We are measuring runtime of how well the server responds to a single request.
+
+        Precondition: ltServer is running.
+        """
+        try:
+            new_client = languagetoolClient.languagetoolClient()
+            with open(LION_TEXT, 'r') as text_file:
+
+                # Grab the text
+                sample_text = text_file.read()
+
+                # Start timing benchmark
+                start = time.process_time()
+                output = new_client.summarizeText(sample_text)
+                end = time.process_time()
+
+                # Show results
+                print("---------[ TIMING BENCHMARK ]---------")
+                print(f"Time Elapsed: {end - start}")
+                print("--------------------------------------")
         except mappingPathError as e:
             self.fail()
 
