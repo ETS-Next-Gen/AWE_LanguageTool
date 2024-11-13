6054dba3d12cc7f0f59d14c23a6079e21664bc23
Bradley Erickson <bbwe24@gmail.com>
2024-11-13 12:43:15 -0500

    updated languagetool server and client initialization

--- awe_languagetool/languagetoolServer.py
+++ awe_languagetool/languagetoolServer.py
@@ -9,6 +9,7 @@ Copyright © 2022. Educational Testing Service. See license file in
 this repository for details.
 '''
 
+import argparse
 import os
 import subprocess
 import awe_languagetool
@@ -16,7 +17,7 @@ import awe_languagetool
 from importlib import resources
 
 
-def runServer(fileName=None, port=8081, config_file="languagetool.cfg"):
+def runServer(port=8081, config_file=None):
     '''
     Runs the LanguageTool server, using `importlib.resources` to find the
     jar file.
@@ -27,21 +28,21 @@ def runServer(fileName=None, port=8081, config_file="languagetool.cfg"):
             resources.files('awe_languagetool').joinpath('LanguageTool5_5').joinpath('languagetool-server.jar')
         ) as LANGUAGE_TOOL_PATH:
         print("Setting Language Path:",  LANGUAGE_TOOL_PATH)
-        MAPPING_PATH = os.path.dirname(LANGUAGE_TOOL_PATH)
 
-    try:
-        os.chdir(MAPPING_PATH)
-        print("Changed Dir to {}".format(MAPPING_PATH))
-    except FileNotFoundError:
-        print("Path not found starting LanguageTool: ", MAPPING_PATH)
-        raise
+    if config_file and not os.path.isfile(config_file):
+        print(f"Error: Configuration file '{config_file}' not found.")
+        sample_config_path = os.path.join(os.path.dirname(__file__), 'languagetool.tmp.cfg')
+        print(f"Please copy the sample configuration file using `cp {sample_config_path} {config_file}`")
+        raise FileNotFoundError(f"Configuration file '{config_file}' does not exist.")
 
     if not config_file:
-        language_tool_command = f"java -cp languagetool-server.jar \
+        print('No configuration file set, continuing without one.')
+        language_tool_command = f"java -cp {LANGUAGE_TOOL_PATH} \
             org.languagetool.server.HTTPServer \
             --port {port} --allow-origin \"*\""
     else:
-        language_tool_command = f"java -cp languagetool-server.jar \
+        print(f'Using configuration file, {config_file}.')
+        language_tool_command = f"java -cp {LANGUAGE_TOOL_PATH} \
             org.languagetool.server.HTTPServer \
             --config {config_file} --port {port} --allow-origin \"*\""
 
@@ -52,4 +53,18 @@ def runServer(fileName=None, port=8081, config_file="languagetool.cfg"):
 
 
 if __name__ == '__main__':
-    runServer()
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Run the LanguageTool server.")
+    parser.add_argument(
+        '--config', type=str, default=None,
+        help='Path to the configuration file for LanguageTool'
+    )
+    parser.add_argument(
+        '--port', type=int, default=8081,
+        help='Port on which to run the LanguageTool server (default: 8081)'
+    )
+
+    args = parser.parse_args()
+
+    # Pass the arguments to runServer
+    runServer(config_file=args.config, port=args.port)
